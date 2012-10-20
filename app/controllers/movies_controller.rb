462eaef2c5175b736c0be5d1889ed349ac8ac5b0
tcosmir <tcosmir@gmail.com>
2012-10-20 22:15:50 +0300

    3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,32 +10,37 @@ class MoviesController < ApplicationController
     
     @all_ratings = Movie.ratings
     
-    @ordered_by = params[:order_by] if params.has_key? 'order_by'
+    if params.has_key? 'order_by'
+      @ordered_by = params[:order_by]
+      session[:ordered_by] = @ordered_by
+    else
+      @ordered_by = session[:ordered_by]
+    end
     
-    @checked_ratings = params[:ratings] if params.has_key? 'ratings'
-    if !@checked_ratings
+    if params.has_key? 'ratings'
+      @checked_ratings = params[:ratings] 
+      session[:checked_ratings] = @checked_ratings
+    elsif session.has_key? 'checked_ratings'
+      @checked_ratings = session[:checked_ratings]
+    else
       @checked_ratings = Movie.ratings
     end
     
-    #nothing selected  session.clear 
-    if params.has_key? 'utf8'
-      session.delete :ordered_by
-      session.delete :checked_ratings
-    end
-
+    #flash[:notice] = "#{@ordered_by} . #{@checked_ratings}"
+    #@ordered_by = session[:ordered_by] if @ordered_by
+    #@checked_ratings = session[:checked_ratings] if @checked_ratings
+    #@checked_ratings = Movie.ratings if @checked_ratings
+    
     # update session from incoming params
-    session[:ordered_by] = @ordered_by if @ordered_by
-    session[:checked_ratings] = @checked_ratings if @checked_ratings
+    #session[:ordered_by] = @ordered_by if @ordered_by
+    #session[:checked_ratings] = @checked_ratings if @checked_ratings
     
-    if !@ordered_by && !@checked_ratings && session[:checked_ratings]
-      @ordered_by = session[:ordered_by] unless @ordered_by
-      @checked_ratings = session[:checked_ratings] unless @checked_ratings
- 
-      flash.keep
-      redirect_to movies_path({order_by: @ordered_by, ratings: @checked_ratings})
-    end
+    #if @ordered_by || @checked_ratings #&& session[:checked_ratings]
+    #  flash.keep
+    #  redirect_to movies_path({order_by: @ordered_by, ratings: @checked_ratings})
+    #end
     
-    if @checked_ratings
+    if @checked_ratings != Movie.ratings
       if @ordered_by
         @movies = Movie.find_all_by_rating(@checked_ratings, :order => "#{@ordered_by} asc")
       else
