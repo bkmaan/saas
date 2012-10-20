7cdb386c2e2f4e669aa5e915b27944961fa9c6e1
tcosmir <tcosmir@gmail.com>
2012-10-20 18:53:13 +0300

    test

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,16 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+  
+  def was_sorted?(column)
+   @ordered_by==column ? 'hilite' : ''
+  end
+
+  def is_selected?(rating)
+   @checked_ratings.include? rating unless !@checked_ratings
+  end
+
+  def create_order_params(column)
+   {order_by: column, ratings: @checked_ratings}
+  end  
 end
