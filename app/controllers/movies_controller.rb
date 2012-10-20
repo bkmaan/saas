c21bd9e4e9e653858ab0b8a2e4800c9df5820a68
tcosmir <tcosmir@gmail.com>
2012-10-21 00:48:04 +0300

    3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,36 +9,31 @@ class MoviesController < ApplicationController
   def index
     
     @all_ratings = Movie.ratings
-    
-    if params.has_key? 'order_by'
+    @redirect = 0
+    if params.has_key? 'order_by' #&& ! (params.has_key? 'ratings')
       @ordered_by = params[:order_by]
       session[:ordered_by] = @ordered_by
+      if ! (params.has_key? 'rr')
+        @redirect = 1
+        flash.keep
+        redirect_to movies_path({order_by: session[:ordered_by], ratings: session[:checked_ratings], rr: 1})
+      end  
     else
       @ordered_by = session[:ordered_by]
     end
     
-    if params.has_key? 'ratings'
+    if params.has_key? 'ratings' #&& ! (params.has_key? 'order_by')
       @checked_ratings = params[:ratings] 
       session[:checked_ratings] = @checked_ratings
+      if ! (params.has_key? 'rr') && @redirect == 0
+        flash.keep
+        redirect_to movies_path({order_by: session[:ordered_by], ratings: session[:checked_ratings], rr: 1}) 
+      end    
     elsif session.has_key? 'checked_ratings'
       @checked_ratings = session[:checked_ratings]
     else
       @checked_ratings = Movie.ratings
     end
-    
-    #flash[:notice] = "#{@ordered_by} . #{@checked_ratings}"
-    #@ordered_by = session[:ordered_by] if @ordered_by
-    #@checked_ratings = session[:checked_ratings] if @checked_ratings
-    #@checked_ratings = Movie.ratings if @checked_ratings
-    
-    # update session from incoming params
-    #session[:ordered_by] = @ordered_by if @ordered_by
-    #session[:checked_ratings] = @checked_ratings if @checked_ratings
-    
-    #if @ordered_by || @checked_ratings #&& session[:checked_ratings]
-    #  flash.keep
-    #  redirect_to movies_path({order_by: @ordered_by, ratings: @checked_ratings})
-    #end
     
     if @checked_ratings != Movie.ratings
       if @ordered_by
